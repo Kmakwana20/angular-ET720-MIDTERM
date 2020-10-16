61bd6677aa9a646d379c6bb4fe59eee5cd1b8b9b
Kmakwana20 <kshitijmak20@gmail.com>
2020-10-16 15:25:58 -0400

    end of midterm

---
+++ src/app/my-resume/resume-work-experince/resume-work-experince.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-work-experince',
+  templateUrl: './resume-work-experince.component.html',
+  styleUrls: ['./resume-work-experince.component.css']
+})
+export class ResumeWorkExperinceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
